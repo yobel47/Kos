d14728f90d36c7a9545344add1f6bf7e11240a8d
Yobel <vega.yobel.2@gmail.com>
2023-01-13 17:57:10 +0700

    Unfinished verification screen

--- app/src/main/java/com/binar/kos/view/ui/verification/VerificationActivity.kt
+++ app/src/main/java/com/binar/kos/view/ui/verification/VerificationActivity.kt
@@ -2,6 +2,7 @@ package com.binar.kos.view.ui.verification
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.binar.kos.R
 
 class VerificationActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
