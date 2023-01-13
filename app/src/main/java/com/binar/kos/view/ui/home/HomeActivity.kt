c6540a0c1d6c35b318419ae7512e6027610b2481
Yobel <vega.yobel.2@gmail.com>
2023-01-13 17:38:20 +0700

    Test homepage build

---
+++ app/src/main/java/com/binar/kos/view/ui/home/HomeActivity.kt
@@ -0,0 +1,12 @@
+package com.binar.kos.view.ui.home
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.binar.kos.R
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
