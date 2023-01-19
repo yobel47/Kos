c87b83fa2713c6b6a5177d54e3073e47ebc5dcb0
ara.boanerges <ara.boanerges@shopee.com>
2023-01-19 08:00:24 +0700

    add auth and user ApiService

--- app/src/main/java/com/binar/kos/network/ApiService.kt
+++ app/src/main/java/com/binar/kos/network/ApiService.kt
@@ -1,12 +1,15 @@
 package com.binar.kos.network
 
-import com.binar.kos.data.remote.request.LoginRequest
-import com.binar.kos.data.remote.request.RegisterRequest
-import com.binar.kos.data.remote.response.LoginResponse
-import com.binar.kos.data.remote.response.RegisterResponse
+import com.binar.kos.data.remote.request.*
+import com.binar.kos.data.remote.response.*
 import retrofit2.Call
 import retrofit2.http.Body
+import retrofit2.http.DELETE
+import retrofit2.http.GET
+import retrofit2.http.Header
+import retrofit2.http.PATCH
 import retrofit2.http.POST
+import retrofit2.http.Path
 
 interface ApiService {
     @POST("auth/login")
@@ -14,4 +17,20 @@ interface ApiService {
 
     @POST("auth/signup")
     suspend fun register(@Body request: RegisterRequest): RegisterResponse
+
+    @POST("auth/forget-password")
+    suspend fun forgotPassword(@Body request: ForgotPasswordRequest): ForgotPasswordResponse
+
+    @POST("auth/change-password")
+    suspend fun changePassword(@Body request: ChangePasswordRequest): ChangePasswordResponse
+
+    @GET("users/{id}")
+    suspend fun getAllUsers(@Path("id") id: Int, @Header("Authorization") authorization: String): UsersListResponse
+
+    @PATCH("users/{id}")
+    suspend fun editUser(@Path("id") id: Int, @Body request: EditUserRequest, @Header("Authorization") authorization: String): UserResponse
+
+    @DELETE("users/{id}")
+    suspend fun deleteUser(@Path("id") id: Int, @Header("Authorization") authorization: String): UserResponse
+
 }
