f8342c85163407e231f6710d791b0256776afc9e
gamalielara <aragamalielboanerges@gmail.com>
2023-02-05 23:02:23 +0700

    add edit profile activity

---
+++ app/src/main/java/com/binar/kos/view/ui/editProfile/EditProfileActivity.kt
@@ -0,0 +1,11 @@
+package com.binar.kos.view.ui.editProfile
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class EditProfileActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
